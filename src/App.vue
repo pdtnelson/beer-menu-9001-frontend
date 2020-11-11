9b03b75d627e32a9ab56cc64c87b10aea1f6839d
Peter Nelson <pdtnelson@gmail.com>
2020-11-10 23:51:00 -0700

    initial commit with some very basic stuff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppBreweryList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppBreweryList from './components/AppBreweryList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppBreweryList
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = Axios.create({
+  // TODO: Get this to work correctly baseURL: process.env.VUE_APP_API_BASE_URL
+  baseURL: 'http://192.168.10.10/api'
+})
 
 new Vue({
   render: h => h(App),
